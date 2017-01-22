8806b43edcda609b1f7388c88f72f6d076b8e580
MisterPeddler <mr.padli@gmail.com>
2017-01-22 12:57:56 -0800

    Incorporated feedback from last project and polished a few things up

--- js/main.js
+++ js/main.js
@@ -1,35 +1,46 @@
-$(function(){
+$(function() {
 
-//regex function for verifying email input
-function isEmail(email) {
-  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  return regex.test(email);
-}
+    //initializes the smooth scrolling plugin
+    smoothScroll.init();
 
-//click handler for subscribe button
-//if empty ask user for valid email address
-//if valid email then thank user for subscribing
-//uses default html5 behaviour for invalid text entry
-$('.subscribe-button').on('click', function(event){
-  var newEmail = $('input').val();
-  if(isEmail(newEmail)){
-    event.preventDefault();
-    $('input').text('');
-    alert("Thanks for subscribing!");
-  }else if(newEmail === ''){
-    event.preventDefault();
-    alert("Please submit a valid email address.");
-  }
-});
+    //regex function for verifying email input
+    function isEmail(email) {
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+    }
 
-//click handler for 'add to cart'
-var cartCount = 0;
-//when any add to cart button is clicked it increments a counter-text
-//which is displayed over the cart logo
-$('.add-to-cart-button').on('click',function(event){
-  $('.cart-counter').css('display','block');
-  cartCount++;
-  $('.counter-text').text(cartCount);
-});
+    //click handler for subscribe button
+    //if empty ask user for valid email address
+    //if valid email then thank user for subscribing
+    //uses default html5 behaviour for invalid text entry
+    $('.subscribe-button').on('click', function(event) {
+
+        var newEmail = $('input').val();
+
+        if (isEmail(newEmail)) {
+
+            event.preventDefault();
+            $('input').val('');
+            alert("Thanks for subscribing!");
+
+        } else if (newEmail === '') {
+
+            event.preventDefault();
+            alert("Please submit a valid email address.");
+
+        }
+    });
+
+    //click handler for 'add to cart'
+    var cartCount = 0;
+    //when any 'add to cart' button is clicked it increments 'counter-text'
+    //which is displayed over the cart logo
+    $('.add-to-cart-button').on('click', function(event) {
+
+        $('.cart-counter').css('display', 'block');
+        cartCount++;
+        $('.counter-text').text(cartCount);
+
+    });
 
 });
