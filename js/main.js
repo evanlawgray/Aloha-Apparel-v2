07c650e5474d40f100b86c9721716d4a9410eaf2
MisterPeddler <mr.padli@gmail.com>
2017-01-21 17:39:08 -0800

    Added js functionality to email subscription form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 $(function(){
 
+function isEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+}
+
+$('button').on('click', function(event){
+
+  var newEmail = $('input').val();
+
+  if(isEmail(newEmail)){
+  event.preventDefault();
+  $('input').text('');
+  prompt("Thanks for subscribing!");
+
+
+  }else if(newEmail === ''){
+    event.preventDefault();
+    alert("Please submit a valid email address.");
+  }
+
+});
 
 
 });
