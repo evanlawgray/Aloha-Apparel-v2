3254b295a831b1b0f7253cb77059dc47bd549e6c
MisterPeddler <mr.padli@gmail.com>
2017-01-21 19:09:23 -0800

    added add to cart counter

--- js/main.js
+++ js/main.js
@@ -1,26 +1,35 @@
 $(function(){
 
+//regex function for verifying email input
 function isEmail(email) {
   var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   return regex.test(email);
 }
 
-$('button').on('click', function(event){
-
+//click handler for subscribe button
+//if empty ask user for valid email address
+//if valid email then thank user for subscribing
+//uses default html5 behaviour for invalid text entry
+$('.subscribe-button').on('click', function(event){
   var newEmail = $('input').val();
-
   if(isEmail(newEmail)){
-  event.preventDefault();
-  $('input').text('');
-  prompt("Thanks for subscribing!");
-
-
+    event.preventDefault();
+    $('input').text('');
+    alert("Thanks for subscribing!");
   }else if(newEmail === ''){
     event.preventDefault();
     alert("Please submit a valid email address.");
   }
-
 });
 
+//click handler for 'add to cart'
+var cartCount = 0;
+//when any add to cart button is clicked it increments a counter-text
+//which is displayed over the cart logo
+$('.add-to-cart-button').on('click',function(event){
+  $('.cart-counter').css('display','block');
+  cartCount++;
+  $('.counter-text').text(cartCount);
+});
 
 });
